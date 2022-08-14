d5cf256a5f4f27549b5d20f8c63c26de03d6acae
Podvalnyy <Alpodv@gmail.com>
2022-08-14 15:54:02 +0300

    add array and final array

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+//При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Инициализация массива строк
+string[] baseArray = { "Sun", "Monday", "Tuersday","Wed", "Thirsday", "Fri", "Sat","1","12","123","1234" };
+int count=0;
+// Вывод начального массива строк baseArray
+Console.WriteLine("["+string.Join(", ",baseArray)+"]");
+
+string[] createFinalArray(string[] baseArray)
+{
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+    if(baseArray[i].Length <= 3)
+        {
+        count++;
+        }
+    }
+    string[] finalArray = new string[count];
+    count = 0;
+    for (int i = 0; i < baseArray.Length; i++)
+    {
+    if(baseArray[i].Length <= 3)
+        {
+        finalArray[count] = baseArray[i];
+        count++;
+        }
+    }
+    return finalArray;
+}
+void printArray(string[] array)
+{
+    Console.WriteLine("["+string.Join(", ",array)+"]");
+}
+printArray(createFinalArray(baseArray));
+
+      
